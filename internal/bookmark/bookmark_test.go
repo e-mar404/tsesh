0a35e2d56dcd638be946001f021e571e3e79457f
e-mar404 <egmarinn@gmail.com>
2026-02-23 22:16:57 -0600

    test add rm

--- internal/bookmark/bookmark_test.go
+++ internal/bookmark/bookmark_test.go
@@ -3,8 +3,20 @@ package bookmark
 import (
 	"errors"
 	"fmt"
+	"os"
+	"slices"
 	"testing"
 )
+
+type action struct {
+	name   string
+	rawUrl string
+}
+
+type AddRemoveTest struct {
+	actions     []action
+	expctedUrls []Bookmark
+}
 
 func TestValidateUrl(t *testing.T) {
 	tt := []struct {
@@ -22,6 +34,89 @@ func TestValidateUrl(t *testing.T) {
 
 		if !errors.Is(got, tc.expected) {
 			fmt.Printf("expected: %v, got: %v\n", tc.expected, got)
+			t.Fail()
+		}
+	}
+}
+
+func TestAddRemoveBookmarks(t *testing.T) {
+	tt := []AddRemoveTest{
+		{
+			actions: []action{
+				{
+					name:   "add",
+					rawUrl: "https://google.com",
+				},
+			},
+			expctedUrls: []Bookmark{
+				{
+					Url: "https://google.com",
+				},
+			},
+		},
+		{
+			actions: []action{
+				{
+					name:   "remove",
+					rawUrl: "https://google.com",
+				},
+			},
+			expctedUrls: nil,
+		},
+		{
+			actions: []action{
+				{
+					name:   "add",
+					rawUrl: "https://google.com/one",
+				},
+				{
+					name:   "add",
+					rawUrl: "https://google.com/two",
+				},
+				{
+					name:   "remove",
+					rawUrl: "google",
+				},
+			},
+			expctedUrls: nil,
+		},
+		{
+			actions: []action{
+				{
+					name:   "add",
+					rawUrl: "https://google.com",
+				},
+				{
+					name:   "add",
+					rawUrl: "https://github.com",
+				},
+				{
+					name:   "remove",
+					rawUrl: "google",
+				},
+			},
+			expctedUrls: []Bookmark{
+				{
+					Url: "https://github.com",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		got := Data{}
+		for _, action := range tc.actions {
+			switch action.name {
+			case "add":
+				got.Add(action.rawUrl)
+			case "remove":
+				got.Remove(action.rawUrl)
+			}
+		}
+
+		cwd, _ := os.Getwd()
+		if !slices.Equal(got[cwd], tc.expctedUrls) {
+			fmt.Printf("expected: %v, got: %v\n:", tc.expctedUrls, got)
 			t.Fail()
 		}
 	}
