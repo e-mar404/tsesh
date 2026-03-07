3e4aff29fb4cea9737d0b855eaa03f5ece814312
e-mar404 <egmarinn@gmail.com>
2026-03-07 15:08:49 -0600

    list: add styling + some std layout

--- cmd/list.go
+++ cmd/list.go
@@ -3,10 +3,25 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/e-mar404/tsesh/internal/bookmark"
 	"github.com/spf13/cobra"
 )
+
+var nameStyle = lipgloss.NewStyle().
+	Background(lipgloss.Color("62")).
+	Foreground(lipgloss.Color("230"))
+
+var urlStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.AdaptiveColor{Light: "#A49FA5", Dark: "#777777"})
+
+var contentStyle = lipgloss.NewStyle().
+	PaddingLeft(1)
+
+var itemStyle = lipgloss.NewStyle().
+	PaddingBottom(1)
 
 var (
 	listCmd = &cobra.Command{
@@ -27,9 +42,23 @@ var (
 				return nil
 			}
 
+			out := strings.Builder{}
 			for i, mark := range list {
-				fmt.Printf("[%d] %s: %s\n", i, mark.Name, mark.Url)
+				content := lipgloss.JoinVertical(lipgloss.Left,
+					nameStyle.Render(mark.Name),
+					urlStyle.Render(mark.Url),
+				)
+
+				item := lipgloss.JoinHorizontal(lipgloss.Top,
+					fmt.Sprintf("[%d]", i),
+					contentStyle.Render(content),
+				)
+
+				str := fmt.Sprintf("%s\n", itemStyle.Render(item))
+				out.WriteString(str)
 			}
+
+			fmt.Print(out.String())
 
 			return nil
 		},
